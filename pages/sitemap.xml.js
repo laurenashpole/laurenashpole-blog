7f2f08adef5d22f872bfea48e834fc4431ddf8ca
laurenashpole <lauren@laurenashpole.com>
2021-07-18 22:43:53 -0400

    Lint sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -10,7 +10,18 @@ export async function getServerSideProps ({ res }) {
       loc: `${process.env.NEXT_PUBLIC_BASE_URL}${post.id_string}${post.slug ? '/' + post.slug : ''}`,
       lastmod: new Date(post.date).toISOString().substring(0, 10)
     };
-  })
+  });
+
+  const getUrls = (posts) => {
+    return posts.map((post) => {
+      return `
+        <url>
+          <loc>${post.loc}</loc>
+          <lastmod>${post.lastmod}</lastmod>
+        </url>
+      `;
+    }).join('');
+  };
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
@@ -20,14 +31,7 @@ export async function getServerSideProps ({ res }) {
         <lastmod>${new Date().toISOString().substring(0, 10)}</lastmod>
       </url>
 
-      ${posts.map((post) => {
-        return `
-          <url>
-            <loc>${post.loc}</loc>
-            <lastmod>${post.lastmod}</lastmod>
-          </url>
-        `;
-      }).join('')}
+      ${getUrls(posts)}
     </urlset>
   `;
 
