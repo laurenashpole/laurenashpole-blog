e56ed8a1f01b9b1ff4f1f32631b2074b1640b456
laurenashpole <lauren@laurenashpole.com>
2022-06-20 18:01:39 -0400

    Uncomment dev code

--- pages/api/og-image.js
+++ pages/api/og-image.js
@@ -71,10 +71,10 @@ export default async function handler (req, res) {
     </html>
   `;
 
-  // if (process.env.NODE_ENV === 'development') {
-  //   res.setHeader('Content-Type', 'text/html');
-  //   return res.end(html);
-  // }
+  if (process.env.NODE_ENV === 'development') {
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(html);
+  }
 
   const browser = await playwright.launchChromium({ headless: true });
   const page = await browser.newPage();
