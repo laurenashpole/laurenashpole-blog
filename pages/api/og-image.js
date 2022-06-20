899200fa1634db8bb362a6a4224910eaced5fcdf
laurenashpole <lauren@laurenashpole.com>
2022-06-20 14:35:10 -0400

    Installed Playwright, started API route

---
+++ pages/api/og-image.js
@@ -0,0 +1,12 @@
+import playwright from 'playwright-aws-lambda';
+
+export default async function handler (req, res) {
+  if (!(req.query || {}).headline || !(req.query || {}).type) {
+    return res.status(422).json({});
+  }
+
+  res.json({
+    headline: req.query.headline,
+    type: req.query.type
+  });
+}
