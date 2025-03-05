7c9c0717385e58547c2a1c8c7442af383dd8bfd1
laurenashpole <lauren@laurenashpole.com>
2025-03-05 04:05:57 -0500

    Added Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/global.scss';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
